50af43b391b2ee8184e9165d32ea5c7e9e478c7a
bhushan <bbhuskade123@gmail.com>
2024-10-14 22:31:47 +0530

    changed

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ import { ValidationPipe } from "@nestjs/common";
 import * as cors from "cors"; // Import cors module
 
 async function bootstrap() {
+  const corsOptions = {
+    origin: "*", // Replace with your allowed origin
+    methods: ["GET", "POST", "OPTIONS"], // Allowed methods
+    credentials: true, // Enable credentials if needed
+  };
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+
+  app.use(cors(corsOptions));
   app.useGlobalPipes(new ValidationPipe());
   // app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3030);
